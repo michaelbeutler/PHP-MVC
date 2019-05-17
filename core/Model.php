d6375b5275486297f24aafec6481759ad1df6750
MichaelBeutler <michael.beutler@gibmit.ch>
2019-05-17 11:56:22 +0200

    refactored model

--- core/Model.php
+++ core/Model.php
@@ -48,10 +48,74 @@ class Model
 
     public function findByID($id)
     {
-        return $this->findFirst(['conditions'=>"id = ?", 'bind' => [$id]]);
+        return $this->findFirst(['conditions' => "id = ?", 'bind' => [$id]]);
     }
 
-    protected function populateObjData($result) {
+    public function save()
+    {
+        $fields = [];
+        foreach ($this->_columnNames as $column) {
+            $fields[$column] = $this->$column;
+        }
+        // determine wheter to update or insert
+        if (property_exists($this, 'id') && $this->id != '') {
+            return $this->update($this->id, $fields);
+        } else {
+            return $this->insert($fields);
+        }
+    }
+
+    public function insert($fields)
+    {
+        if (empty($fields)) return false;
+        return $this->_db->insert($this->_table, $fields);
+    }
+
+    public function update($id, $fields)
+    {
+        if (empty($fields) || $id = '') return false;
+        return $this->_db->update($this->_table, $id, $fields);
+    }
+
+    public function delete($id = '')
+    {
+        if ($id == '' && $this->id == '') return false;
+        $id = ($id == '') ? $this->id : $id;
+        if ($this->_softDelete) {
+            return $this->update($id, ['deleted' => 1]);
+        }
+        return $this->_db->delete($this->_table, $id);
+    }
+
+    public function query($sql, $bind = [])
+    {
+        return $this->_db->query($sql, $bind);
+    }
+
+    public function data()
+    {
+        $data = new stdClass();
+        foreach($this->_columnNames as $column) {
+            $data->column = $this->column;
+        }
+        return $data;
+    }
+
+    public function assign($params)
+    {
+        if (!empty($params)) {
+            foreach ($params as $key => $value) {
+                if (in_array($key, $this->_columnNames)) {
+                    $this->$key = sanitize($value);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    protected function populateObjData($result)
+    {
         foreach ($result as $key => $value) {
             $this->$key = $value;
         }
